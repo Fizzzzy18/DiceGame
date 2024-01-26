4120109d96e303643745d58c38a6b993dff57036
Fizzzzy18 <singhpranay111@gmail.com>
2024-01-26 20:01:35 +0530

    Initial Upload

---
+++ src/components/RollDice.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const RollDice = ({ rollDice, currentDice }) => {
+
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={rollDice}>
+        <img src={`/dice/dice_${currentDice}.png`} />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+export default RollDice;
+
+const DiceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 48px;
+
+  p {
+    font-size: 24px;
+  }
+  .dice {
+    cursor: pointer;
+  }
+`;
