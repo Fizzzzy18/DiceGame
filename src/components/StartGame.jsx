317c1e946f08346350e5da7be8f0a12f299fc723
Pranay Singh <143025694+Fizzzzy18@users.noreply.github.com>
2024-01-27 23:12:25 +0530

    Update StartGame.jsx

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -8,7 +8,7 @@ const StartGame = ({ toggle }) => {
   return (
     <Container>
         <div>
-       <img src="./images/dices.png" />
+       <img src="/images/dices.png" />
 
         </div>
         <div className="content">
